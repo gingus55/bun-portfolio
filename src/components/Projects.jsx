0632af3d586c85eb29b10c98b1421202bca97a31
gingus55 <gingus55@hotmail.com>
2023-09-22 13:49:23 +0100

    project framework added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,15 @@
 const Projects = () => {
-  return <div className="bg-slate-400 mx-2 h-96 flex mt-2">Projects</div>;
+  return (
+    <div className="bg-slate-400 mx-2  h-96 flex flex-col mt-2">
+      <div className="max-w-full h-48 bg-yellow-400 m-2">Main</div>
+      <div className="max-w-full bg-red-200 mx-2 flex">
+        <div className="bg-blue-200 w-1/2">1</div>
+        <div className="bg-green-200 w-1/2">2</div>
+        <div className="bg-purple-300 w-1/2">3</div>
+        <div className="bg-orange-400 w-1/2">4</div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
