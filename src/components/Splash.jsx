bb91f59504097bd99192f04ca84b507ec75022c2
gingus55 <gingus55@hotmail.com>
2023-09-22 12:46:58 +0100

    overlow dealt with

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -1,11 +1,11 @@
 export default function Splash() {
   return (
     <div className="bg-slate-400 mx-2 h-96 flex">
-      <div className="bg-red-400 h-96 w-1/3 flex justify-center px-2 items-center">
+      <div className="bg-red-400 h-96 w-1/3 flex overflow-hidden justify-center px-2 items-center">
         <img src="/icarus.jpg" />
       </div>
-      <div className="bg-blue-200 h-96 w-2/3 flex text-center items-center px-4">
-        <p>
+      <div className=" h-96 w-2/3 flex text-center items-center px-4">
+        <p className="font-bold">
           Maths teacher turned Full Stack Developer with The University of
           Birmingham. Always had a passion for coding since writing boot-disks
           for games on my 486!
