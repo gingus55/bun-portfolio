d663a955d3f79226fce3b9f3511ea1819e65d09f
gingus55 <gingus55@hotmail.com>
2023-09-23 10:31:42 +0100

    skill miages linked

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,17 @@
 const Skills = () => {
   return (
-    <div className="bg-slate-400 mx-2  h-96 flex flex-col mt-2">Skills</div>
+    <div className="bg-slate-400 mx-2  h-96 flex flex-row mt-2">
+      <img src="./js-logo.png" className="w-14 h-14" />
+      <img src="./html-logo-2.png" className="w-14 h-14" />
+      <img src="./css-img.png" className="w-14 h-14" />
+      <img src="./git-img.png" className="w-14 h-14" />
+      <img src="./jquery-logo.png" className="w-14 h-14" />
+      <img src="./mysql-img.png" className="w-14 h-14" />
+      <img src="./node-js-img.png" className="w-14 h-14" />
+      <img src="./vite.svg" className="w-14 h-14" />
+      <img src="./tailwindcss.png" className="w-14 h-14" />
+      <img src="./bun.svg" className="w-14 h-14" />
+    </div>
   );
 };
 
