ffd0e477135d90280471c1dee0c872dc2796429f
gingus55 <gingus55@hotmail.com>
2023-10-01 17:47:26 +0100

    items centered

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,14 @@
 const Projects = () => {
   return (
     <div className="bg-slate-400 mx-2  min-h-fit flex flex-col mt-2">
-      <div className="max-w-full min-h-fit bg-yellow-400 flex-col justify-around align-center m-2">
+      <div className="max-w-full min-h-fit flex items-center bg-yellow-400 flex-col justify-around align-center m-2">
         <img className="w-5/6" src="./sas-screenshot.png" />
       </div>
-      <div className="max-w-full min-h-fit bg-red-200 mx-2 flex">
-        <div className="bg-blue-200 w-1/2">
+      <div className="max-w-full min-h-fit  bg-red-200 mx-2 flex">
+        <div className="bg-blue-200 flex flex-col items-center w-1/2">
           <img src="./fit-screenshot.png" className="w-5/6" />
         </div>
-        <div className="bg-green-200 w-1/2">
+        <div className="bg-green-200 w-1/2 flex flex-col items-center">
           <img src="./emptrack-screenshot.png" className="w-5/6" />
         </div>
       </div>
