2b126a648a75bb17054cfc6d4a27759bcf8283d5
gingus55 <gingus55@hotmail.com>
2023-10-01 17:43:43 +0100

    3image links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,18 @@
 const Projects = () => {
   return (
-    <div className="bg-slate-400 mx-2  h-96 flex flex-col mt-2">
-      <div className="max-w-full h-screen bg-yellow-400 m-2">Main</div>
-      <div className="max-w-full h-screen bg-red-200 mx-2 flex">
-        <div className="bg-blue-200 w-1/2">1</div>
-        <div className="bg-green-200 w-1/2">2</div>
+    <div className="bg-slate-400 mx-2  min-h-fit flex flex-col mt-2">
+      <div className="max-w-full min-h-fit bg-yellow-400 flex-col justify-around align-center m-2">
+        <img className="w-5/6" src="./sas-screenshot.png" />
       </div>
-      <div className="max-w-full h-screen bg-red-300 mx-2 flex">
+      <div className="max-w-full min-h-fit bg-red-200 mx-2 flex">
+        <div className="bg-blue-200 w-1/2">
+          <img src="./fit-screenshot.png" className="w-5/6" />
+        </div>
+        <div className="bg-green-200 w-1/2">
+          <img src="./emptrack-screenshot.png" className="w-5/6" />
+        </div>
+      </div>
+      <div className="max-w-full min-h-fit bg-red-300 mx-2 flex">
         <div className="bg-purple-300 w-1/2 px-1">3</div>
         <div className="bg-orange-400 w-1/2 px-1">4</div>
       </div>
