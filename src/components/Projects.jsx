fc175941e37664e9f63f507c9d9b94648e4380d5
gingus55 <gingus55@hotmail.com>
2023-10-03 11:19:36 +0100

    hover added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,14 +2,23 @@ const Projects = () => {
   return (
     <div className="bg-slate-400 mx-2  min-h-fit flex flex-col mt-2">
       <div className="max-w-full min-h-fit flex items-center bg-yellow-400 flex-col justify-around align-center m-2">
-        <img className="w-5/6" src="./sas-screenshot.png" />
+        <img
+          className="w-5/6 hover:scale-105 hover:shadow-xl transition-transform duration-500"
+          src="./sas-screenshot.png"
+        />
       </div>
       <div className="max-w-full min-h-fit  bg-red-200  my-1 mx-1 flex">
         <div className="bg-blue-200 flex mx-1 flex-col items-center w-1/2">
-          <img src="./fit-screenshot.png" className="w-5/6" />
+          <img
+            src="./fit-screenshot.png"
+            className="w-5/6 hover:scale-105 hover:shadow-xl transition-transform duration-500"
+          />
         </div>
         <div className="bg-green-200 mx-1 w-1/2 flex flex-col items-center">
-          <img src="./emptrack-screenshot.png" className="w-5/6" />
+          <img
+            src="./emptrack-screenshot.png"
+            className="w-5/6 hover:scale-105 hover:shadow-xl transition-transform duration-500"
+          />
         </div>
       </div>
       <div className="max-w-full min-h-fit bg-red-300 mx-1 my-1 flex">
